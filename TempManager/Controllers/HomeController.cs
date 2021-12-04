d1f81a70af6509fd59de7edd68f820d91bac42c2
salingmw <58449342+salingmw@users.noreply.github.com>
2021-12-03 22:38:40 -0500

    Updated Models, Controllers, Views

--- TempManager/Controllers/HomeController.cs
+++ TempManager/Controllers/HomeController.cs
@@ -15,11 +15,72 @@ namespace Ch11Ex1TempManager.Controllers
             return View(temps);
         }
 
+        //Entree
         [HttpGet]
-        public ViewResult Add() => View(new Temp());
+        public ViewResult AddEntree() => View(new Temp());
 
         [HttpPost]
-        public IActionResult Add(Temp temp)
+        public IActionResult AddEntree(Temp temp)
+        {
+            // server-side check for remote validation for duplicate date
+            Temp check = data.Temps.FirstOrDefault(t => t.Date == temp.Date);
+            if (check != null)
+            {
+                ModelState.AddModelError("Date",
+                    $"The date {temp.Date?.ToShortDateString()} is already in the database.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                data.Temps.Add(temp);
+                data.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // model-level validation message
+                ModelState.AddModelError("", "Please correct all errors.");
+                return View(temp);
+            }
+        }
+
+        //Sides
+        [HttpGet]
+        public ViewResult AddSide() => View(new Temp());
+
+        [HttpPost]
+        public IActionResult AddSide(Temp temp)
+        {
+            // server-side check for remote validation for duplicate date
+            Temp check = data.Temps.FirstOrDefault(t => t.Date == temp.Date);
+            if (check != null)
+            {
+                ModelState.AddModelError("Date",
+                    $"The date {temp.Date?.ToShortDateString()} is already in the database.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                data.Temps.Add(temp);
+                data.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // model-level validation message
+                ModelState.AddModelError("", "Please correct all errors.");
+                return View(temp);
+            }
+        }
+
+        //Desserts
+        [HttpGet]
+        public ViewResult AddDessert() => View(new Temp());
+
+        [HttpPost]
+        public IActionResult AddDessert(Temp temp)
         {
             // server-side check for remote validation for duplicate date
             Temp check = data.Temps.FirstOrDefault(t => t.Date == temp.Date);
@@ -45,14 +106,46 @@ namespace Ch11Ex1TempManager.Controllers
         }
 
         [HttpGet]
-        public ViewResult Delete(int id)
+        public ViewResult DeleteEntree(int id)
         {
             var temp = data.Temps.Find(id);
             return View(temp);
         }
 
         [HttpPost]
-        public RedirectToActionResult Delete(Temp temp)
+        public RedirectToActionResult DeleteEntree(Temp temp)
+        {
+            data.Remove(temp);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ViewResult DeleteSide(int id)
+        {
+            var temp = data.Temps.Find(id);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult DeleteSide(Temp temp)
+        {
+            data.Remove(temp);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ViewResult DeleteDessert(int id)
+        {
+            var temp = data.Temps.Find(id);
+            return View(temp);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult DeleteDessert(Temp temp)
         {
             data.Remove(temp);
             data.SaveChanges();
---
+++ TempManager/Models/Entree.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TempManager.Models
+{
+    public class Entree
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter date added.")]
+        [Remote("CheckDate", "Validation")]
+        public DateTime? Date { get; set; }
+
+        [Required(ErrorMessage = "Please enter a Name for your Entree.")]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
