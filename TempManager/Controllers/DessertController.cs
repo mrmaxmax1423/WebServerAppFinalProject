7c0a07bf74448a88549f4e894f5034d985cb6617
salingmw <58449342+salingmw@users.noreply.github.com>
2021-12-05 19:38:51 -0500

    Added controllers and views
    Added controllers and views for Entree side and Desserts

---
+++ TempManager/Controllers/DessertController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class DessertController : Controller
+    {
+        private DishesContext data { get; set; }
+        public DessertController(DishesContext ctx) => data = ctx;
+        public ViewResult Index()
+        {
+            var desserts = data.Desserts.OrderBy(t => t.Date).ToList();
+            return View(desserts);
+        }
+
+        [HttpGet]
+        public ViewResult AddDessert() => View(new Dessert());
+
+        [HttpPost]
+        public IActionResult AddDessert(Dessert dessert)
+        {
+            data.Desserts.Add(dessert);
+            data.SaveChanges();
+            return View(dessert);
+        }
+
+        [HttpGet]
+        public ViewResult DeleteDessert(int id)
+        {
+            var dessert = data.Desserts.Find(id);
+            return View(dessert);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult DeleteDessert(Dessert dessert)
+        {
+            data.Remove(dessert);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TempManager/Controllers/SideController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class SideController : Controller
+    {
+        private DishesContext data { get; set; }
+        public SideController(DishesContext ctx) => data = ctx;
+        public ViewResult Index()
+        {
+            var sides = data.Sides.OrderBy(t => t.Date).ToList();
+            return View(sides);
+        }
+
+        [HttpGet]
+        public ViewResult AddSide() => View(new Side());
+
+        [HttpPost]
+        public IActionResult AddSide(Side side)
+        {
+            data.Sides.Add(side);
+            data.SaveChanges();
+            return View(side);
+        }
+
+        [HttpGet]
+        public ViewResult DeleteSide(int id)
+        {
+            var side = data.Sides.Find(id);
+            return View(side);
+        }
+
+        [HttpPost]
+        public RedirectToActionResult DeleteSide(Side side)
+        {
+            data.Remove(side);
+            data.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
